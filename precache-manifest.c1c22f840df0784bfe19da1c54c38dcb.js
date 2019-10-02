2dccebd1ca9d20d087aada571b649f703710c692
Akeundo <48980681+Akeundo@users.noreply.github.com>
2019-10-02 23:46:09 +0300

    Updates

---
+++ precache-manifest.c1c22f840df0784bfe19da1c54c38dcb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "84683f052878691113a7581990c663c6",
+    "url": "/static/media/profilePic.84683f05.jpg"
+  },
+  {
+    "revision": "b53b31066ca393114279c263277eb7a9",
+    "url": "/static/media/background2.b53b3106.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2106923bef4d7fc2ffb0",
+    "url": "/static/js/main.2106923b.chunk.js"
+  },
+  {
+    "revision": "44160a888c0bd273c417",
+    "url": "/static/js/1.44160a88.chunk.js"
+  },
+  {
+    "revision": "2106923bef4d7fc2ffb0",
+    "url": "/static/css/main.37522aae.chunk.css"
+  },
+  {
+    "revision": "44160a888c0bd273c417",
+    "url": "/static/css/1.04952b42.chunk.css"
+  },
+  {
+    "revision": "1d72106f71dd018893611554e167e446",
+    "url": "/index.html"
+  }
+];
